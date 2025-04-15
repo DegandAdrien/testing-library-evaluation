33c38071c9af2ae4003e456c980b70237b2dc113
Adrien Degand <adrien.degand@hotmail.com>
2025-04-15 10:27:56 +0200

    Create second scenario

--- src/__tests__/app.jsx
+++ src/__tests__/app.jsx
@@ -78,3 +78,50 @@ test('Premier scénario : cas passant', async () => {
     expect(screen.getByText(/Welcome home/i)).toBeInTheDocument()
   })
 })
+
+test('Second scénario : cas non passant', async () => {
+  render(<App />)
+  expect(screen.getByText(/Welcome home/i)).toBeInTheDocument()
+  expect(screen.getByText(/Fill out the form/i)).toBeInTheDocument()
+
+  userEvent.click(screen.getByText(/Fill out the form/i))
+
+  await waitFor(() => {
+    expect(screen.getByText(/Page 1/i)).toBeInTheDocument()
+  })
+
+  expect(screen.getByText(/Go Home/i)).toBeInTheDocument()
+  expect(screen.getByLabelText(/Favorite Food/i)).toBeInTheDocument()
+
+  userEvent.type(screen.getByLabelText(/Favorite Food/i), '')
+
+  expect(screen.getByText(/Next/i)).toBeInTheDocument()
+  userEvent.click(screen.getByText(/Next/i))
+
+  await waitFor(() => {
+    expect(screen.getByText(/Page 2/i)).toBeInTheDocument()
+  })
+
+  expect(screen.getByText(/Go Back/i)).toBeInTheDocument()
+  expect(screen.getByLabelText(/Favorite Drink/i)).toBeInTheDocument()
+
+  userEvent.type(screen.getByLabelText(/Favorite Drink/i), 'Bière')
+
+  expect(screen.getByText(/Review/i)).toBeInTheDocument()
+  userEvent.click(screen.getByText(/Review/i))
+
+  await waitFor(() => {
+    expect(screen.getByRole('heading', {name: 'Confirm'})).toBeInTheDocument()
+  })
+
+  expect(screen.getByText(/Please confirm your choices/i)).toBeInTheDocument()
+
+  expect(screen.getByLabelText('Favorite Food')).toHaveTextContent('')
+  expect(screen.getByLabelText('Favorite Drink')).toHaveTextContent('Bière')
+
+  expect(screen.getByText(/Go Back/i)).toBeInTheDocument()
+  const confirmButton = screen.getByRole('button', {name: 'Confirm'})
+  expect(confirmButton).toBeInTheDocument()
+
+  userEvent.click(confirmButton)
+})
