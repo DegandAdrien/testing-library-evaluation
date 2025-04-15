60d58795069edcfe71acb03295e35bcca7610cda
Adrien Degand <adrien.degand@hotmail.com>
2025-04-15 09:18:01 +0200

    Add step 5 & 6 from first scenario

--- src/__tests__/app.jsx
+++ src/__tests__/app.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
-import {render, screen} from '@testing-library/react'
+import {render, screen, waitFor} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import App from '../app'
 
-test('Premier scénario : cas passant', () => {
+test('Premier scénario : cas passant', async () => {
   render(<App />)
   expect(screen.getByText(/Welcome home/i)).toBeInTheDocument()
   expect(screen.getByText(/Fill out the form/i)).toBeInTheDocument()
 
   userEvent.click(screen.getByText(/Fill out the form/i))
+
+  await waitFor(() => {
+    expect(screen.getByText(/Page 1/i)).toBeInTheDocument()
+  })
 })
